7fba4940e2f94ef8086535e6bf5ca1de04b6e80d
Dalisson Figueiredo <dalissonfigueiredo@gmail.com>
2021-10-02 00:11:52 -0300

    manager is added when project is created

---
+++ challengeProject/Repository/Implementations/EmployeeRepositoryImplementation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using challengeProject.Model;
+using challengeProject.Model.Context;
+using challengeProject.Repository.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace challengeProject.Repository.Implementations
+{
+    public class EmployeeRepositoryImplementation : GenericRepository<Employee>
+    {
+        private MySQLContext _context;
+        private DbSet<Employee> dataset;
+        public EmployeeRepositoryImplementation(MySQLContext context) : base(context)
+        {
+            _context = context;
+            dataset = _context.Set<Employee>();
+        }
+
+        public Membership AddToProject(Membership membership)
+        {
+            MembershipRepositoryImplementation memship = new MembershipRepositoryImplementation(_context);
+
+            return memship.Create(membership);
+        }
+        
+    }
+}
